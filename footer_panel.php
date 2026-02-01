bd009beb66023ea81e17340fa6db91e935371f2c
EseSphere <info@esesphere.com>
2026-02-01 09:07:27 +0000

    Updates: 2026-02-01T09:07:24.565Z

--- footer_panel.php
+++ footer_panel.php
@@ -144,6 +144,10 @@ $result = $conn->query($sql);
 <script src="js/bootstrap.bundle.min.js"></script>
 <script src="js/jquery.easing.min.js"></script>
 <script src="js/scripts.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
 </body>
 
 </html>
