e2c56caf1c20eb4414f25433724b54cbaf04d2b7
EseSphere <info@esesphere.com>
2026-02-01 09:11:47 +0000

    Updates: 2026-02-01T09:11:44.936Z

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $result = mysqli_query($conn, $sql);
                     <h1>Full Stack Web Developer</h1>
                 </div>
                 <div class="text-container" data-aos="zoom-in" data-aos-duration="2000">
-                    <h2 class="h1-large">Crafting elegant and high-performance websites with passion</h2>
+                    <h2 class="h2-large">Crafting elegant and high-performance websites with passion</h2>
                     <a class="btn btn-info btn-lg page-scroll text-decoration-none" href="#about">Learn More</a>
                     <a class="btn btn-success btn-lg page-scroll text-decoration-none" href="#contact">
                         Get in Touch
